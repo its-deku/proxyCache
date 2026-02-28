ecf833b573900f5f7fdffe3423ced4e9700510f8
its-deku <deku77mha@gmail.com>
2026-03-01 01:05:10 +0530

    print cache hit/miss messages on terminal

--- http_handler/handler.go
+++ http_handler/handler.go
@@ -1,7 +1,7 @@
 package httphandler
 
 import (
-	"io"
+	"fmt"
 	"log"
 	"net/http"
 	"strings"
@@ -24,21 +24,22 @@ func Init(port string, forwardUrl string) {
 }
 
 func (pry *proxy) Handle(w http.ResponseWriter, req *http.Request) {
-	newUrl := pry.URL + strings.TrimLeft(req.URL.Path, "/")
+	newUrl := pry.URL + strings.TrimLeft(req.URL.String(), "/")
 
 	// check if the url exists in the cache
 	if _, exists := pry.cache[newUrl]; exists {
 		pry.cache[newUrl].Header.Set("X-Cache", "HIT")
-		io.WriteString(w, pry.cache[newUrl].Header["X-Cache"][0]+"\t")
-		io.WriteString(w, pry.cache[newUrl].Status)
+		fmt.Print("X-Cache: ", pry.cache[newUrl].Header["X-Cache"][0]+"  ")
+		fmt.Println(pry.cache[newUrl].Status)
 	} else {
 		res, err := http.Get(newUrl)
 		if err != nil {
 			panic(err)
 		}
+		defer res.Body.Close()
 		res.Header.Add("X-Cache", "MISS")
-		io.WriteString(w, res.Header["X-Cache"][0]+"\t")
-		io.WriteString(w, res.Status)
+		fmt.Print("X-Cache: ", res.Header["X-Cache"][0]+"  ")
+		fmt.Println(res.Status)
 
 		// cache the response (add concurrency safety)
 		pry.muLock.Lock()
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 
 	httphandler "example.com/v2/proxy_cache/http_handler"
 )
@@ -10,6 +11,8 @@ func main() {
 	port := flag.String("port", "8080", "port on which the server runs")
 	url := flag.String("origin", "", "URL for the intended website for which the caching should be enabled")
 	flag.Parse()
+
+	fmt.Printf("Proxy server started on port %s\n", *port)
 
 	httphandler.Init(*port, *url)
 }
